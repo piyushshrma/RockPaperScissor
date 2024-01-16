72aab034e95f6fd4cddb0dbb6cd18c590b332a28
Piyush Sharma <141904179+piyushshrma@users.noreply.github.com>
2024-01-16 22:37:48 +0530

    Added sound effect

--- RockPaperScissor.js
+++ RockPaperScissor.js
@@ -7,65 +7,55 @@ const msg = document.querySelector("#msg");
 const userScorePara = document.querySelector("#user-score");
 const compScorePara = document.querySelector("#comp-score");
 
+// Use the Audio constructor to create an audio element for the common sound
+const commonSound = new Audio("sound.mp3");
+
 const getCompChoice = () => {
-    //rock, paper or scissor
     const options = ["rock", "paper", "scissor"];
     const randomIdx = Math.floor(Math.random() * 3);
     return options[randomIdx];
 }
 
-const showWinner = (userWin, userChoice, compChoice) =>{
-    if(userWin){
+const showWinner = (userWin, userChoice, compChoice) => {
+    if (userWin) {
         userScore++;
         userScorePara.innerText = userScore;
         msg.innerText = `You Won! ${userChoice} beats ${compChoice}`;
         msg.style.backgroundColor = "green";
-    }
-    else{
+    } else {
         compScore++;
         compScorePara.innerText = compScore;
         msg.innerText = `You Lost! Try Again ${compChoice} beats your ${userChoice}`;
         msg.style.backgroundColor = "red";
     }
+
+    // Play the common sound after each game (win or lose)
+    commonSound.play();
 }
 
-const playGame = (userChoice) =>{
-    console.log(`User choice = ${userChoice}`);
-    //Generating computer choice
+const playGame = (userChoice) => {
     const compChoice = getCompChoice();
-    console.log(`Computer choice = ${compChoice}`);
 
-    if(userChoice===compChoice){
-        //Draw
+    if (userChoice === compChoice) {
         console.log("Game is draw.");
         msg.innerText = "Game was draw! Play Again";
         msg.style.backgroundColor = "#081b31";
-    }
-
-    else{
-        let userWin=true;
-        if(userChoice==="rock"){
-            //comp has scissor or paper
+    } else {
+        let userWin = true;
+        if (userChoice === "rock") {
             userWin = compChoice === "paper" ? false : true;
-        } 
-        else if(userChoice==="paper"){
-            //comp has rock or scissor
+        } else if (userChoice === "paper") {
             userWin = compChoice === "scissor" ? false : true;
-        }
-        else{
-            //comp has rock or paper
+        } else {
             userWin = compChoice === "rock" ? false : true;
         }
         showWinner(userWin, userChoice, compChoice);
     }
 };
 
-choices.forEach((choice)=>{
-    console.log(choice);
-    choice.addEventListener("click",() => {
+choices.forEach((choice) => {
+    choice.addEventListener("click", () => {
         const userChoice = choice.getAttribute("id");
         playGame(userChoice);
     });
 });
-
-
